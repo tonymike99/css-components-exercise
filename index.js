b3876ab0cbb225783dbd2fbd477aa0d8ea0e0366
Tony Mike Auxen <tonymike99@gmail.com>
2022-01-24 17:12:21 +0530

    code: made changes to modal component

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ document
     .getElementById('modal-open-btn')
     .addEventListener('click', function () {
         document.getElementById('modal-open-btn').style.visibility = 'hidden';
+        // document.getElementsByTagName('body')[0].style.opacity = 0.5;
         document
             .getElementsByClassName('modal__component')[0]
             .classList.add('active');
@@ -10,9 +11,9 @@ document
 document
     .getElementById('modal-return-btn')
     .addEventListener('click', function () {
-        document.getElementById('modal-return-btn').style.display = 'block';
+        document.getElementById('modal-open-btn').style.visibility = 'visible';
+        // document.getElementsByTagName('body')[0].style.opacity = 1;
         document
             .getElementsByClassName('modal__component')[0]
             .classList.remove('active');
-        document.getElementById('modal-open-btn').style.visibility = 'visible';
     });
