f85d2b19e4bfd1cbc947f7f1f0ae99a43f51c62e
Tony Mike Auxen <tonymike99@gmail.com>
2022-01-24 20:56:03 +0530

    fix: modal fixed position behaviour

--- modal.js
+++ modal.js
@@ -2,6 +2,8 @@ document
     .getElementById('modal-open-btn')
     .addEventListener('click', function () {
         document.getElementById('modal-open-btn').style.visibility = 'hidden';
+        document.getElementsByClassName('modal__component')[0].style.display =
+            'block';
         // document.getElementsByTagName('body')[0].style.opacity = 0.5;
         document
             .getElementsByClassName('modal__component')[0]
@@ -12,6 +14,8 @@ document
     .getElementById('modal-return-btn')
     .addEventListener('click', function () {
         document.getElementById('modal-open-btn').style.visibility = 'visible';
+        document.getElementsByClassName('modal__component')[0].style.display =
+            'none';
         // document.getElementsByTagName('body')[0].style.opacity = 1;
         document
             .getElementsByClassName('modal__component')[0]
